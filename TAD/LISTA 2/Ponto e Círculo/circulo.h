4d8e70279347489b5c96e19d566eb4a57f133831
Everton Teixeira <evertonteixe99@alu.ufc.br>
2023-08-12 20:36:27 -0300

    add lista 2(questão 1, 2a e 2b)

---
+++ TAD/LISTA 2/Ponto e Círculo/circulo.h
@@ -0,0 +1,27 @@
+#include "ponto.h"
+#define PI 3.14
+
+//Arquivo de protótipo de circulo.c
+
+//Tipo exportado
+typedef struct circulo Circulo;
+
+//Funções exportadas
+
+/*Criando*/
+Circulo* circ_cria(float x, float y, float r);
+
+/*Libera a memória*/
+void circ_libera(Circulo *c);
+
+/*Retorna a área do circulo*/
+float circ_area(Circulo *c);
+
+/*Verifica se o ponto está dentro do circulo*/
+int circ_interior(Circulo *c, Ponto *p);
+
+/*Retorna a circunferência de um circulo (lista 2 questão 1a)*/
+float circunferencia(Circulo *c);
+
+/*Alterar raio do circulo (alternativa 'b' da questão 2 lista2)*/
+void alteraRaio(Circulo *c, float r);
---
+++ TAD/LISTA 2/Ponto e Círculo/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "circulo.h"
+#include "ponto.h"
+
+int main(){
+
+    //criando um círculo
+
+    Circulo *meuCirculo;
+    meuCirculo = circ_cria(0.0, 0.0, 3.0);
+
+    //testando a função da área do círculo
+    float area = circ_area(meuCirculo);
+    printf("\nA area do circulo e: %.1f", area);
+
+    //mostrar se um ponto está dentro do circulo
+
+    //criando um ponto para teste:
+    Ponto *ponto;
+
+    ponto = cria(1.0, 2.0);
+
+    /*mostrar coordenada do ponto criado (questão 1 da lista2)*/
+    mostraCoordenadas(ponto);
+
+    // Testando a função circ_interior
+    if(circ_interior(meuCirculo, ponto)){
+        printf("\nO ponto esta dentro do circulo");
+    }else {
+        printf("\nO ponto não esta dentro do circulo");
+    }
+
+    /*Testando a alternativa 'a' da questão 2*/
+    printf("\nA circunferencia do circulo e: %.2f", circunferencia(meuCirculo));
+
+    // Liberando memória
+    circ_libera(meuCirculo);
+    libera(ponto);
+
+    return 0;
+}
